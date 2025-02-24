bb7756fc0fcae534a3795da1d5b0a68489664fee
SAJJAD HOSAIN JIM <sajjadjim15@gmail.com>
2025-02-25 01:45:50 +0600

    new commit done add

---
+++ script/addmoney.js
@@ -0,0 +1,33 @@
+document
+  .getElementById("add-money-submit")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    // console.log("working button")
+    // pin access that
+    const pin = document.getElementById("pin-number").value;
+    //   converted pin int number
+    const convertedPIN = parseInt(pin);
+
+    //AMount take
+    const amount = document.getElementById("add-amount").value;
+    const convertedAMOUNT = parseInt(amount);
+    // Get the Main balance Here
+    const main_Balance = document.getElementById("main-balance").innerText;
+    const mainBalanceConvert = parseFloat(main_Balance);
+    // check that the balance
+    if (amount && pin) {
+      if (convertedPIN === 1234) {
+        console.log(mainBalanceConvert);
+        const sum = mainBalanceConvert + convertedAMOUNT;
+        document.getElementById("main-balance").innerText = sum;
+      } else {
+        alert("Enter valid PIN !!!");
+      }
+    } else {
+      alert("Enter amount !!!");
+    }
+  });
+// logout Button work 
+  document.getElementById('log-out').addEventListener('click',function(){
+    window.location.href="./index.html"
+})
